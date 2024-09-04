225029aab6f42b99b33e25d82d023116c445ae3d
ExionDev <chall@exion.xyz>
2024-09-04 20:10:42 +0100

    Ken listener

--- listeners/jamesListener.js
+++ listeners/jamesListener.js
@@ -7,7 +7,7 @@ new Listener({
     event: 'messageDelete',
 
     run: async ctx => {
-        if (ctx.member.id === "1276867467708207114" || ctx.member.id === "1220936359397822485") {
+        if (ctx.member.id === "1276867467708207114" || ctx.member.id === "1220936359397822485" || ctx.member.id === "1265921550029164544") {
             // Check if webhook exists and create it if it doesn't (with same avatar and name)
             let jamesWebhook = new WebhookClient({ url: "https://discord.com/api/webhooks/1280965607818268786/AxtekFIux2x2NPpwvD5EeglIFmZxezB4BAkTHTPWm26hxz8vbHJc0EtQLeYvpo1032a5" })
             console.log(jamesWebhook.id)
