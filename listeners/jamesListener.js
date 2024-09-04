382436c2d07a5e72d4e5d60264ea7ac1ac824c51
ExionDev <chall@exion.xyz>
2024-09-04 20:01:15 +0100

    James

--- listeners/jamesListener.js
+++ listeners/jamesListener.js
@@ -1,5 +1,6 @@
 // Check if discord message is deleted is by user id 1276867467708207114, then send webhook message with same avatar and content
 const { Listener } = require('gcommands');
+const { WebhookClient } = require('discord.js')
 
 new Listener({
     name: 'James',
@@ -8,17 +9,10 @@ new Listener({
     run: async ctx => {
         if (ctx.member.id === "1276867467708207114") {
             // Check if webhook exists and create it if it doesn't (with same avatar and name)
-            const webhook = await ctx.channel.fetchWebhooks();
-            let suggestionWebhook = webhook.find(webhook => webhook.name === ctx.member.displayName);
-            if (!suggestionWebhook) {
-                console.log(ctx.member.displayName)
-                suggestionWebhook = await ctx.channel.createWebhook(ctx.member.displayName, {
-                    avatar: ctx.member.displayAvatarURL(),
-                    channel: ctx.channel.id
-                });
-            }
+            let jamesWebhook = new WebhookClient({ url: "https://discord.com/api/webhooks/1280965607818268786/AxtekFIux2x2NPpwvD5EeglIFmZxezB4BAkTHTPWm26hxz8vbHJc0EtQLeYvpo1032a5" })
+
             // Send the message through webhook
-            await suggestionWebhook.send({
+            await jamesWebhook.send({
                 content: ctx.content,
                 username: ctx.member.displayName,
                 avatarURL: ctx.member.displayAvatarURL()
