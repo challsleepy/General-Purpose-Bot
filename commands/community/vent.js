55c60e5b899cfd467351cdf57658cfa4b11ba958
ExionDev <chall@exion.xyz>
2024-10-23 20:43:26 +0100

    Vent command

---
+++ commands/community/vent.js
@@ -0,0 +1,43 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const { EmbedBuilder } = require('discord.js');
+
+new Command({
+    name: 'vent',
+    description: 'Vent anonymously!',
+    type: [CommandType.SLASH],
+    arguments: [
+        new Argument({
+            name: 'content',
+            description: 'What would you like to say?',
+            type: ArgumentType.STRING,
+            required: true
+        }),
+    ],
+
+    run: async (ctx) => {
+        try {
+            await ctx.deferReply({ ephemeral: true })
+
+            const content = ctx.arguments.getString('content');
+
+            const ventEmbed = new EmbedBuilder()
+                .setColor("Random")
+                .setTitle(`VENT`)
+                .setDescription(`*${content}*`)
+                .setFooter({ text: "Anonymous", iconURL: ctx.client.user.displayAvatarURL() })
+                .setTimestamp()
+
+            await ctx.interaction.channel.send({
+                embeds: [ventEmbed],
+                ephemeral: false,
+            });
+
+            ctx.editReply("Your vent has been sent!")
+
+        } catch (error) {
+            // Handle any errors that occur
+            console.error('⚠️ Error handling document:', error);
+            await ctx.interaction.reply({ content: '⚠️ Error occurred during vent.', ephemeral: true });
+        }
+    }
+})
