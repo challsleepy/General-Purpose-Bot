dbacb8f2dd85378c77bd7011a7b0963edf939711
Chall <a.ahmedtws23173@gmail.com>
2024-11-19 15:12:26 +0100

    Changed vent command to confess command. Made it so confessions can only be sent to a set channel

--- commands/community/confess.js
+++ commands/community/confess.js
@@ -1,14 +1,19 @@
+/**
+ * Command to send an anonymous message to the channel, useful for "confessions".
+ *
+ */
 const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
 const { EmbedBuilder } = require('discord.js');
+const config = require('../../config.json')
 
 new Command({
-    name: 'vent',
-    description: 'Vent anonymously',
+    name: 'confess',
+    description: 'Confess anonymously',
     type: [CommandType.SLASH],
     arguments: [
         new Argument({
             name: 'content',
-            description: 'Let it all out',
+            description: '👀',
             type: ArgumentType.STRING,
             required: true
         }),
@@ -16,28 +21,35 @@ new Command({
 
     run: async (ctx) => {
         try {
+            if (ctx.channel.id !== config.discord.confessionsChannelId) {
+                return ctx.reply({ content: `This command can only be used in <#${config.discord.confessionsChannelId}>.`, ephemeral: true });
+            }
+            // Defer the reply so the user doesn't see the typing indicator
             await ctx.deferReply({ ephemeral: true })
 
             const content = ctx.arguments.getString('content');
 
             const ventEmbed = new EmbedBuilder()
                 .setColor("Random")
-                .setTitle(`VENT`)
+                .setTitle(`CONFESSION`)
                 .setDescription(`*${content}*`)
                 .setFooter({ text: "Anonymous", iconURL: ctx.client.user.displayAvatarURL() })
                 .setTimestamp()
 
+            // Send the embed to the channel
             await ctx.interaction.channel.send({
                 embeds: [ventEmbed],
                 ephemeral: false,
             });
 
-            ctx.editReply("Your vent has been sent!")
+            // Let the user know that the message has been sent
+            ctx.editReply("Your confession has been sent!")
 
         } catch (error) {
             // Handle any errors that occur
-            console.error('⚠️ Error handling document:', error);
-            await ctx.interaction.reply({ content: '⚠️ Error occurred during vent.', ephemeral: true });
+            console.error(' Error handling document:', error);
+            await ctx.interaction.reply({ content: ' Error occurred during confession.', ephemeral: true });
         }
     }
 })
+
