c1e2dfbd8a6ffc714cf753549e81d64f58834f13
Chall <a.ahmedtws23173@gmail.com>
2025-03-01 00:07:01 +0100

    - Updated member joined embed color

--- listeners/serverJoin.js
+++ listeners/serverJoin.js
@@ -4,33 +4,34 @@ const config = require("../config.json");
 
 // Listener to check if user joined the server and send a welcome message with png attachment
 new Listener({
-	name: "Server Welcome",
-	event: "guildMemberAdd",
-	run: async (ctx) => {
-		if (ctx.user.bot) return;
+  name: "Server Welcome",
+  event: "guildMemberAdd",
+  run: async (ctx) => {
+    if (ctx.user.bot) return;
 
-		try {
-			const welcomeChannel = await ctx.guild.channels.fetch(config.discord.welcomeChannelId)
+    try {
+      const welcomeChannel = await ctx.guild.channels.fetch(
+        config.discord.welcomeChannelId
+      );
 
-			const embed = new EmbedBuilder()
-				.setTitle(`Welcome to ${ctx.guild.name}!`)
-				.setDescription(`Hey <@${ctx.user.id}>!`)
-				.setImage("attachment://banner.png")
-				.setColor("#ffbf00")
-				.setTimestamp();
+      const embed = new EmbedBuilder()
+        .setTitle(`Welcome to ${ctx.guild.name}!`)
+        .setDescription(`Good to see you <@${ctx.user.id}>!`)
+        .setImage("attachment://banner.png")
+        .setColor("#6488ea")
+        .setTimestamp();
 
-			await welcomeChannel.send({
-				embeds: [embed],
-				files: [
-					{
-						attachment:
-							"./utils/assets/codenchillbanner.png",
-						name: "banner.png",
-					},
-				],
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	},
+      await welcomeChannel.send({
+        embeds: [embed],
+        files: [
+          {
+            attachment: "./utils/assets/codenchillbanner.png",
+            name: "banner.png",
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 });
